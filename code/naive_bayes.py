e67034db68930c05737dac52fb41551254dcf51c
jeremysu090400 <131489422+jeremysu090400@users.noreply.github.com>
2026-04-30 10:46:37 -0500

    Code formatting
    Refactored function definition to a single line.

--- code/naive_bayes.py
+++ code/naive_bayes.py
@@ -1,11 +1,8 @@
 from sklearn.naive_bayes import MultinomialNB
 
 
-def get_naive_bayes_predictions(
-    X_train,
-    y_train,
-    X_test,
-):
+def get_naive_bayes_predictions(X_train, y_train, X_test):
+    """Train Multinomial Naive Bayes and return class predictions and fake-news probabilities."""
     model = MultinomialNB(alpha=1.0)
 
     model.fit(X_train, y_train)
